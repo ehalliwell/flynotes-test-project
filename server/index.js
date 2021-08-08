e9813d694588761aba0e11162021b6d9c8093263
TRT-ElliotH <elliot.halliwell@bt.com>
2021-08-08 21:13:12 +0100

    Remove comment

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const cors = require("cors");
-const auth = require("./auth")
+const auth = require("./auth");
 const bcrypt = require("bcrypt");
 const cors = require("cors");
 
